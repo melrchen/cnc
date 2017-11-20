cc2bafd640736f3c0d77dc62de7c64c01ee0d3b2
emcheng1 <emcheng1@yahoo.com>
2017-11-20 15:57:28 -0500

    Finalized featurization for classifiers

--- featurization.py
+++ featurization.py
@@ -7,40 +7,58 @@ from skimage import io
 import matplotlib.pyplot as plt
 import argparse
 import cv2
+from sklearn.decomposition import PCA
+
+def PCAthatbitch(X):
+	'''
+	Applies PCA to a matrix of features X and returns the PCA'd X.
+	'''
+	pca = PCA(n_components = 30)
+	Y = pca.fit_transform(X)
+
+	return Y
 
 
- 
-# load the image and convert it to a floating point data type
-#image = img_as_float(io.imread(args["image"]))
-image = cv2.imread('doggo.png')
-yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+def featurize(filename):
+	'''
+	Returns FFT features of an image given by filename.
 
-gray_padded = np.pad(gray, [(7, 7), (7, 7)], mode='constant')
+	Args:
+		filename (String)
 
-feature_vector = []
+	Returns:
+		feature_vector (np.array): Matrix of feature vectors corresponding to image pixels.
+	'''
+	# load the image and convert it to a floating point data type
+	#image = img_as_float(io.imread(args["image"]))
+	image = cv2.imread(filename)
+	yuv = cv2.cvtColor(image, cv2.COLOR_BGR2YUV)
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 
-for ii in range(0, gray.shape[0]):
-    row = []
-    for jj in range(0, gray.shape[1]):
-        pixel = (ii + 7, jj + 7)
+	gray_padded = np.pad(gray, [(7, 7), (7, 7)], mode='constant')
 
-        square = gray_padded[pixel[0]-7 : pixel[0]+8, pixel[1]-7 : pixel[1]+8]
-        #print square.shape
-        #surf = cv2.SURF(.1)
-        #kp, des = surf.detectAndCompute(square,None)
-        features = np.fft.fft2(square).flatten()
-        features = np.append(features, np.mean(square))
-        features = np.append(features, np.std(square))
+	feature_vector = []
 
-        #print features.shape
-        row.append(features)
-    feature_vector.append(np.array(row))
-feature_vector = np.array(feature_vector)
+	for ii in range(0, gray.shape[0]):
+	    for jj in range(0, gray.shape[1]):
+	        pixel = (ii + 7, jj + 7)
+
+	        square = gray_padded[pixel[0]-7 : pixel[0]+8, pixel[1]-7 : pixel[1]+8]
+	        #print square.shape
+	        #surf = cv2.SURF(.1)
+	        #kp, des = surf.detectAndCompute(square,None)
+	        features = np.fft.fft2(square).flatten()
+	        features = np.append(features, np.mean(square))
+	        features = np.append(features, np.std(square))
+
+	        feature_vector.append(features)
+	    
+	feature_vector = np.array(feature_vector)
+	feature_vector = PCAthatbitch(feature_vector)
 
 
-print feature_vector.shape
+	return feature_vector
 #surf = cv2.SURF(500)
 #kp, des = surf.detectAndCompute(image,None)
 
@@ -48,3 +66,6 @@ print feature_vector.shape
 
 #img2 = cv2.drawKeypoints(square,kp,None,(255,0,0),4)
 #plt.imshow(img2),plt.show()
+
+if __name__ == "__main__":
+	print(featurize('doggo.png').shape)
